6b7bb22b662b6624ce0955df45aa88b48f1cc2b9
Keiko Raiola <keikotr@hawaii.edu>
2023-11-27 23:48:21 -1000

    Changed the "create club" button to "update club" + created new edit club components.

--- app/imports/ui/pages/EditClub.jsx
+++ app/imports/ui/pages/EditClub.jsx
@@ -4,86 +4,83 @@ import { AutoForm, TextField, LongTextField, SubmitField, ErrorsField, SelectFie
 import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
+import { useParams } from 'react-router';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { _ } from 'meteor/underscore';
-// eslint-disable-next-line no-unused-vars
-import { addProjectMethod } from '../../startup/both/Methods';
-import { Interests } from '../../api/interests/Interests';
-import { Profiles } from '../../api/profiles/Profiles';
-import { ProfilesInterests } from '../../api/profiles/ProfilesInterests';
-import { ProfilesProjects } from '../../api/profiles/ProfilesProjects';
-import { Projects } from '../../api/projects/Projects';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { pageStyle } from './pageStyles';
 import { ComponentIDs, PageIDs } from '../utilities/ids';
+import { Clubs } from '../../api/clubs/Clubs';
+import { ClubsInterests } from '../../api/clubs/ClubsInterests';
+// import { updateClubMethod } from '../../startup/both/Methods';
+import { Interests } from '../../api/interests/Interests';
 
 /* Create a schema to specify the structure of the data to appear in the form. */
-const makeSchema = (allInterests, allParticipants) => new SimpleSchema({
-  name: String,
+const makeSchema = (allInterests) => new SimpleSchema({
+  clubName: String,
   description: String,
-  homepage: String,
-  picture: String,
+  contact: String,
   interests: { type: Array, label: 'Interests', optional: false },
   'interests.$': { type: String, allowedValues: allInterests },
-  participants: { type: Array, label: 'Participants', optional: true },
-  'participants.$': { type: String, allowedValues: allParticipants },
 });
 
 /* Renders the Page for adding a project. */
-const CreateClub = () => {
+const EditClub = () => {
 
+  const { _id } = useParams();
+  const { ready, interests, clubData } = useTracker(() => {
+    // Ensure that minimongo is populated with all collections prior to running render().
+    const sub1 = Meteor.subscribe(Interests.userPublicationName);
+    const sub2 = Meteor.subscribe(Clubs.userPublicationName);
+    const sub3 = Meteor.subscribe(ClubsInterests.userPublicationName);
+    const clubInfo = Clubs.collection.findOne(_id);
+    return {
+      ready: sub1.ready() && sub2.ready() && sub3.ready(),
+      interests: Interests.collection.find().fetch(),
+      clubData: clubInfo,
+    };
+  }, [_id]);
   /* On submit, insert the data. */
   const submit = (data, formRef) => {
-    // eslint-disable-next-line no-undef
-    Meteor.call(CreateClubMethod, data, (error) => {
+    const { clubName, contact, description } = data;
+    Clubs.collection.update({ clubName }, { $set: { clubName, contact, description } }, (error) => {
       if (error) {
         swal('Error', error.message, 'error');
       } else {
-        swal('Success', 'Project added successfully', 'success').then(() => formRef.reset());
+        swal('Success', 'Club created successfully', 'success').then(() => formRef.reset());
       }
     });
+    ClubsInterests.collection.remove({ club: clubName });
+    interests.map((interest) => ClubsInterests.collection.insert({ club: clubName, interest }));
   };
-
-  const { ready, interests, profiles } = useTracker(() => {
-    // Ensure that minimongo is populated with all collections prior to running render().
-    const sub1 = Meteor.subscribe(Interests.userPublicationName);
-    const sub2 = Meteor.subscribe(Profiles.userPublicationName);
-    const sub3 = Meteor.subscribe(ProfilesInterests.userPublicationName);
-    const sub4 = Meteor.subscribe(ProfilesProjects.userPublicationName);
-    const sub5 = Meteor.subscribe(Projects.userPublicationName);
-    return {
-      ready: sub1.ready() && sub2.ready() && sub3.ready() && sub4.ready() && sub5.ready(),
-      interests: Interests.collection.find().fetch(),
-      profiles: Profiles.collection.find().fetch(),
-    };
-  }, []);
-
+  console.log('clubData:', clubData);
   let fRef = null;
   const allInterests = _.pluck(interests, 'name');
-  const allParticipants = _.pluck(profiles, 'email');
-  const formSchema = makeSchema(allInterests, allParticipants);
+  const formSchema = makeSchema(allInterests);
   const bridge = new SimpleSchema2Bridge(formSchema);
   const transform = (label) => ` ${label}`;
   /* Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   return ready ? (
     <Container style={pageStyle}>
-      <Row id={PageIDs.CreateClubPage} className="justify-content-center">
+      <Row id={PageIDs.editClubPage} className="justify-content-center">
         <Col xs={10}>
           <Col className="text-center"><h2>Edit Club</h2></Col>
-          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)} model={clubData}>
             <Card>
               <Card.Body>
                 <Row>
-                  <Col xs={6}><TextField id={ComponentIDs.CreateClubFormName} label="Name of the club" name="name" showInlineError /></Col>
+                  <Col xs={6}><TextField id={ComponentIDs.editClubFormName} label="Name of the club" name="clubName" showInlineError />
+                    <TextField id={ComponentIDs.editClubFormContact} label="Contact" name="contact" />
+                  </Col>
                 </Row>
                 <Row>
-                  <Col xs={6} id={ComponentIDs.CreateClubFormInterests}>
+                  <Col xs={6} id={ComponentIDs.editClubFormInterests}>
                     <SelectField label="Related interests: (Select that apply)" name="interests" showInlineError placeholder="Interests" multiple checkboxes transform={transform} />
                   </Col>
-                  <LongTextField id={ComponentIDs.CreateClubFormDescription} label="Description" name="description" />
+                  <LongTextField id={ComponentIDs.editClubFormDescription} label="Description" name="description" />
                 </Row>
-                <SubmitField id={ComponentIDs.CreateClubFormSubmit} value="Create Club" />
+                <SubmitField id={ComponentIDs.editClubFormSubmit} value="Update Club" />
                 <ErrorsField />
               </Card.Body>
             </Card>
@@ -94,4 +91,4 @@ const CreateClub = () => {
   ) : <LoadingSpinner />;
 };
 
-export default CreateClub;
+export default EditClub;
