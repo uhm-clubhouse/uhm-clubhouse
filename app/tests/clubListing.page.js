e641aa42dee00a119a049e2781a5befbc4743c62
Miki Kojima <miki615@hawaii.edu>
2023-12-09 22:27:19 -1000

    modified search testing to search for 'club'

--- app/tests/clubListing.page.js
+++ app/tests/clubListing.page.js
@@ -24,7 +24,7 @@ class ClubListingPage {
   /** Checks that the search function works */
   async goToSearch(testController) {
     await this.isDisplayed(testController);
-    const interest = ' ';
+    const interest = 'club';
     const searchInput = Selector(`#${ComponentIDs.clubListingSearch}`);
     await testController.typeText(searchInput, interest);
     const cardCount = Selector('.card').count;
